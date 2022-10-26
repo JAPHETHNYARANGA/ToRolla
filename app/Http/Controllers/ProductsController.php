bea2c111121302451c36cb9ad5af888f2d50e68b
JAPHETHNYARANGA <nyaranga4@gmail.com>
2022-10-26 22:03:12 +0300

    created sample products model

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,7 +6,26 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    public function addProduct(){
-        echo 'product added successfully';
+    public function addProduct(Request $request){
+        $request->validate([
+            'productName' =>'required',
+            'productDescription' =>'required',
+            'productLocation' =>'required',
+            'productImage' =>'required',
+        ]);
+        $user = new Product();
+         $user ->user_id = $uuid;
+        $user -> name = $request->name;
+        $user -> email = $request->email;
+        $user -> phone_number = $request->phone_number;
+        $user -> password =Hash::make($request->password);
+        $res = $user->save();
+        if($res){
+            return redirect('login')->with('success', 'you have registered successfully');
+        }else{
+            return back() ->with('fail', 'Something went wrong. Please try again');
+        }
     }
+
+    
 }
---
+++ app/Models/Products.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    protected $table = "products";
+
+    protected $fillable = ['user_id', 'product_name','product_location','product_description','product_category'];
+}
